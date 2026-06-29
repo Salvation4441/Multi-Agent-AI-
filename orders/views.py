095d06615d1cd7f21cf5e52d6488104ed7ddf83e
Salvation4441 <moprhinerichard1@gmail.com>
2026-06-29 13:09:23 +0000

    feat: integrate and display existing support conversation history in order detail view

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Order, RefundRequest
 from django.contrib.auth.decorators import login_required
+from support.models import Conversation, Message
 
 @login_required
 # Create your views here.
@@ -23,9 +24,20 @@ def order_detail(request, order_id):
     # get refund request for this order if it exists
     refunds = RefundRequest.objects.filter(order=order)
 
+    # getting conversation and messages
+    try:
+        conversation = Conversation.objects.get(user=request.user, order=order)
+        previous_messages = conversation.messages.order_by("created_at")
+
+    except Conversation.DoesNotExist:
+        conversation = None
+        previous_messages = []
+
     context = {
         "order": order,
         "refunds": refunds,
+        "conversation" : conversation,
+        "previous_messages" : previous_messages,
     }
 
     return render(request, "order_detail.html", context)
