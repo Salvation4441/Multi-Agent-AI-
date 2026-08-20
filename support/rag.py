ca7bcc6f7d3c1b9cf2c07a2237d865ee64e93825
Salvation4441 <moprhinerichard1@gmail.com>
2026-08-20 11:53:43 +0000

    refactor: rename embedding_function variable to embedding_func in rag.py

--- support/rag.py
+++ support/rag.py
@@ -7,13 +7,12 @@ from chromadb.utils.embedding_functions import DefaultEmbeddingFunction
 # initialize chromadb client
 client  =  chromadb.PersistentClient(path="./chroma_db")
 
-embedding_function = DefaultEmbeddingFunction()
+embedding_func = DefaultEmbeddingFunction()
 
 # create collection
 collection  = client.get_or_create_collection(
     name = "coolbreeze_docs",
-    embedding_function= embedding_function
+    embedding_function= embedding_func
 )
-
 
 
